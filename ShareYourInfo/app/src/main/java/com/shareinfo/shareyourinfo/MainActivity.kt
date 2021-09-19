4d1a38f1eb9c991ed5d592a5bee98e2c0ac6a452
barto <bartoszreterski94@gmail.com>
2021-09-19 13:20:51 +0200

    rozpoczecie prac

---
+++ ShareYourInfo/app/src/main/java/com/shareinfo/shareyourinfo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shareinfo.shareyourinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
